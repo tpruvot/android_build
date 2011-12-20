f81c75ed355b43f461642a44ca7e5d8c91b1e725
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-12-20 23:15:23 +0100

    repogc shortcut
    Change-Id: If4da9b09d1288d1a02eda7e23e55d787093cd762

--- envsetup.sh
+++ envsetup.sh
@@ -1484,6 +1484,11 @@ function reposync() {
             ;;
     esac
 }
+
+function repogc() {
+    repo forall -c "git gc"
+}
+
 # Force JAVA_HOME to point to java 1.6 if it isn't already set
 function set_java_home() {
     if [ ! "$JAVA_HOME" ]; then
